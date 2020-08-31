9ff5c2ca6cf7338154c9c1b53126058774ed2d68
Abulu, Oseme <osemeabulu@yahoo.com>
2020-08-31 10:38:41 -0400

    Added a setup method for RecieptPrinterTest

--- src/test/java/dojo/supermarket/model/ReceiptPrinterTest.java
+++ src/test/java/dojo/supermarket/model/ReceiptPrinterTest.java
@@ -2,25 +2,42 @@ package dojo.supermarket.model;
 
 import dojo.supermarket.ReceiptPrinter;
 import org.approvaltests.Approvals;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ReceiptPrinterTest {
+
+    private FakeCatalog catalog;
+    private Teller teller;
+    private ShoppingCart cart;
+    private ReceiptPrinter receiptPrinter;
+
+    @Before
+    public void setUp() throws Exception {
+
+        catalog = new FakeCatalog();
+        teller = new Teller(catalog);
+        cart = new ShoppingCart();
+        receiptPrinter = new ReceiptPrinter();
+    }
+
     @Test
     public void cartWithNoOffersTest() {
-        var catalog = new FakeCatalog();
         Product cheerios = new Product("Cheerios", ProductUnit.Each);
         Product cornflakes = new Product("Cornflakes", ProductUnit.Each);
-        catalog.addProduct(cheerios, 1.99);
         catalog.addProduct(cornflakes, 2.56);
-        var teller = new Teller(catalog);
-        var cart = new ShoppingCart();
+        catalog.addProduct(cheerios, 1.99);
         cart.addItem(cheerios);
         cart.addItem(cornflakes);
-        var receiptPrinter = new ReceiptPrinter();
 
         Receipt receipt = teller.checksOutArticlesFrom(cart);
         String receiptString = receiptPrinter.printReceipt(receipt);
 
         Approvals.verify(receiptString);
+    }
+
+    @Test
+    public void cartWithOneSpecialOffer() {
+
     }
 }
